70548ba3a046d9a62ab69cb62d5d7612452cb2d9
Jonas Kempe <landberg@live.se>
2023-02-17 23:04:51 +0100

    passing initial pre-commit including lint

--- car.py
+++ car.py
@@ -1,8 +1,16 @@
-class Car():
+"""Module containing the class Car
+"""
+
+
+class Car:
+    """Class to hold parameters and functions of a car"""
+
     def __init__(self, name: str) -> None:
         self.name: str = name
         self.brand: str = ""
-        
 
     def __str__(self) -> str:
         return f"car named: {self.name}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
--- tco.py
+++ tco.py
@@ -1,11 +1,14 @@
+"""Module used to calculate TCO for cars specified in yaml files
+    """
+import yaml  # pylint: disable=import-error
 from car import Car
-import yaml
 
 
-def main():
+def main() -> None:
+    """main function"""
     mycar: Car = Car("test_car")
     print(mycar)
-    with open("./configs/test_car.yaml", "r") as stream:
+    with open("./configs/test_car.yaml", encoding="utf-8") as stream:
         mycar2 = yaml.safe_load(stream)
     print(mycar2)
 
